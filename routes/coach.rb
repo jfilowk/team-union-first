20f973d37bb056d393d3b030591539c7f608fce8
Javier García Hormigo <javier.tb@outlook.com>
2014-10-12 01:39:17 +0200

    CRUD done. Fetch errors.

--- routes/coach.rb
+++ routes/coach.rb
@@ -1,51 +1,38 @@
 # encoding: UTF-8
 
-helpers do
-  def json_status(code, reason)
-     status code
-      {
-        :status => code,
-        :reason => reason
-      }.to_json
-  end
-end
-
-get 'api/coach/:id' do
+get '/api/coach/:id' do
   content_type :json
   
-  if coach = Coach.get(params[:id])
+  coach = Coach.get(params[:id])
   coach.to_json  
-  else
-    json_status 404 "Not Found"
+
 end
 
-post 'api/coach/new' do
+post '/api/coach/new' do
   content_type :json
   
   coach_json = JSON.parse request.body.read
   coach = Coach.new(coach_json)
-  if coach.save
-  else
-    json_status 404 "Not Found"
+  coach.save
   
 end
 
-put 'api/coach/edit' do
+put '/api/coach/edit' do
   content_type :json
+  coach_json = JSON.parse request.body.read
+  
+    coach = Coach.get(coach_json["id"])
+    coach = Coach.update(
+    firstName: coach_json["firstName"],
+    lastName: coach_json["lastName"],
+    password: coach_json["password"]
+    )
 
-  if coach = Coach.get(params[:id])
-    coach
-    coach.save
-  else
-    json_status 404 "Not Found"
 end
-
-delete 'api/coach/:id' do
+delete '/api/coach/:id' do
   content_type :json
   
-  if coach = Coach.get(params[:id])
-    coach.destroy
-  else
-    json_status 404 "Not Found"
+    coach = Coach.get(params[:id])
+    coach.destroy!
+  
 end
-
